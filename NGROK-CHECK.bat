703b0cf5701285a759a4221ef7ad56597974cea8
awesome-john <80100838+awesome-john@users.noreply.github.com>
2021-03-06 15:50:37 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -1,11 +1,11 @@
 @echo off
 diskperf -Y >nul
 sc start audiosrv >nul
-net user VPS VPSAsian123 /add >nul
-net localgroup administrators VPS /add >nul
-net user VPS /active:yes >nul
+net user hoangnvm MinhHoang123 /add >nul
+net localgroup administrators hoangnvm /add >nul
+net user hoangnvm /active:yes >nul
 echo All done! Connect your VM using RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret"
-echo User: VPS
-echo Pass: VPSAsian123
+echo User: hoangnvm
+echo Pass: MinhHoang123
